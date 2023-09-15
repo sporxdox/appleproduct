06c37dc5c3a432b97de708763c91b9fa573b4cf3
sporxdox <56952828+sporxdox@users.noreply.github.com>
2023-09-15 16:38:01 +0700

    sql data

---
+++ appleproduct.sql
@@ -0,0 +1,19 @@
+/*
+-- Query: SELECT * FROM appledb.apple
+LIMIT 0, 1000
+
+-- Date: 2023-09-15 16:32
+*/
+INSERT INTO `` (`id`,`product_name`,`series`,`product_series`,`storage`,`price`) VALUES (1,'Iphone',15,NULL,'128GB',32900);
+INSERT INTO `` (`id`,`product_name`,`series`,`product_series`,`storage`,`price`) VALUES (2,'Iphone',15,NULL,'256GB',36900);
+INSERT INTO `` (`id`,`product_name`,`series`,`product_series`,`storage`,`price`) VALUES (3,'Iphone',15,NULL,'512GB',45900);
+INSERT INTO `` (`id`,`product_name`,`series`,`product_series`,`storage`,`price`) VALUES (4,'Iphone',15,'Plus','128GB',37900);
+INSERT INTO `` (`id`,`product_name`,`series`,`product_series`,`storage`,`price`) VALUES (5,'Iphone',15,'Plus','256GB',41900);
+INSERT INTO `` (`id`,`product_name`,`series`,`product_series`,`storage`,`price`) VALUES (6,'Iphone',15,'Plus','512GB',50900);
+INSERT INTO `` (`id`,`product_name`,`series`,`product_series`,`storage`,`price`) VALUES (7,'Iphone',15,'Pro','128GB',41900);
+INSERT INTO `` (`id`,`product_name`,`series`,`product_series`,`storage`,`price`) VALUES (8,'Iphone',15,'Pro','256GB',45900);
+INSERT INTO `` (`id`,`product_name`,`series`,`product_series`,`storage`,`price`) VALUES (9,'Iphone',15,'Pro','512GB',54900);
+INSERT INTO `` (`id`,`product_name`,`series`,`product_series`,`storage`,`price`) VALUES (10,'Iphone',15,'Pro','1TB',63900);
+INSERT INTO `` (`id`,`product_name`,`series`,`product_series`,`storage`,`price`) VALUES (11,'Iphone',15,'Pro Max','256GB',48900);
+INSERT INTO `` (`id`,`product_name`,`series`,`product_series`,`storage`,`price`) VALUES (12,'Iphone',15,'Pro Max','512GB',57900);
+INSERT INTO `` (`id`,`product_name`,`series`,`product_series`,`storage`,`price`) VALUES (13,'Iphone',15,'Pro Max','1TB',66900);
